bced0be7e2024f5347f6d2d9b1882db8b2c4149e
luongtan204 <luongtan204>
2025-04-09 01:11:54 +0700

    Connect API - Datatable (get)

--- Tuan7/my-app/src/components/Main.jsx
+++ Tuan7/my-app/src/components/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
 import Overview from '../components/OverView';
+import Database from '../components/Database';
 const Main = () => {
 
     return (
-        <><Header></Header><Overview></Overview></>
+        <><Overview></Overview></>
     );
 }
 export default Main;
